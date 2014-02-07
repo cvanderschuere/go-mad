0c2c70140e7cd80460fe3acf8b9806dc8da9d981
cvanderschuere <christopher.vanderschuere@gmail.com>
2014-02-07 05:27:09 +0000

    Removed some data copying. Non-functional

--- mad.go
+++ mad.go
@@ -10,18 +10,23 @@ import "C"
 import(
 	"io"
 	"unsafe"
+	"reflect"
 	"fmt"
-	"encoding/binary"
 	"math"
 	"bytes"
+	"errors"
 )
 
 type Decoder struct {
 	input io.Reader
-	output chan int16
+	output *bytes.Buffer
 	
 	buf []byte
 	done bool
+	
+	inputCount int
+	outputCount int
+	dither *audio_dither
 	
 	//Audio Information
 	SampleRate uint
@@ -31,6 +36,9 @@ type Decoder struct {
 	// C internals
 	decoder *C.struct_mad_decoder
 }
+
+var ErrNoInput = errors.New("No Input")
+var ErrDecoding = errors.New("Decoding")
 
 const(
 	BUF_SIZE = 1048576
@@ -44,9 +52,10 @@ func New(i io.Reader)(*Decoder,error){
 	
 	size := int(unsafe.Sizeof(C.struct_mad_decoder{}))
 	d.decoder = (*C.struct_mad_decoder)(C.malloc(C.size_t(size)))
-	d.output = make(chan int16, 50000) //random value for buffer
+	d.output = new(bytes.Buffer)
 	d.buf = make([]byte,BUF_SIZE)
 	d.done = false
+	d.dither = &audio_dither{}
 		
 	C.setupDecoder(d.decoder,unsafe.Pointer(d))
 	
@@ -57,7 +66,6 @@ func New(i io.Reader)(*Decoder,error){
 			fmt.Println("Decoder Error")
 		}
 		C.mad_decoder_finish(d.decoder)	
-		close(d.output)
 	}()
 
 	return d,nil
@@ -65,6 +73,16 @@ func New(i io.Reader)(*Decoder,error){
 
 // Read decoded bytes from input (buffered)
 func (d *Decoder) Read(p []byte) (n int, err error){
+	
+	if d.inputCount == 0{
+		return 0,ErrNoInput
+	}else if d.outputCount == 0{
+		return 0,ErrDecoding
+	}else{
+		return d.output.Read(p)
+	}
+	
+	/*
 	//Make buffers
 	buff := new(bytes.Buffer)
 	
@@ -87,6 +105,7 @@ func (d *Decoder) Read(p []byte) (n int, err error){
 	copy(p,buff.Bytes())
 	
 	return len(p),nil
+	*/
 }
 //
 // Callbacks
@@ -101,8 +120,7 @@ func inputFunc(data unsafe.Pointer, stream *C.struct_mad_stream)(C.enum_mad_flow
 	}
 		
 	//Calculate bytes to preserve
-	//uintptr(unsafe.Pointer(p))
-        bytes_to_preserve := uintptr(unsafe.Pointer(stream.bufend)) - uintptr(unsafe.Pointer(stream.next_frame));
+	bytes_to_preserve := uintptr(unsafe.Pointer(stream.bufend)) - uintptr(unsafe.Pointer(stream.next_frame));
 		
 	//Copy over remaining bytes of this frame
 	var bytesPreserved []byte
@@ -110,13 +128,11 @@ func inputFunc(data unsafe.Pointer, stream *C.struct_mad_stream)(C.enum_mad_flow
 		bytesPreserved = C.GoBytes(unsafe.Pointer(stream.next_frame), C.int(bytes_to_preserve))
 		copy(d.buf,bytesPreserved)
 	}
-	
 				
 	if readCount,err := d.input.Read(d.buf[len(bytesPreserved):]); err != nil{
 		if err == io.EOF{
 			fmt.Println("Ended reading")
 			d.done = true
-			
 		}else{	
 			fmt.Println(err)
 			return C.MAD_FLOW_BREAK
@@ -126,6 +142,8 @@ func inputFunc(data unsafe.Pointer, stream *C.struct_mad_stream)(C.enum_mad_flow
 		
 		//Read valid information
 		C.mad_stream_buffer(stream,(*C.uchar)(unsafe.Pointer(&d.buf[0])), C.ulong(readCount+len(bytesPreserved)))
+		
+		d.inputCount += readCount+len(bytesPreserved)
 	}
 	
 	return C.MAD_FLOW_CONTINUE	
@@ -148,26 +166,24 @@ func outputFunc(data unsafe.Pointer, header *C.struct_mad_header,pcm *C.struct_m
 	        
 	length := int(pcm.length)
 	
-	lSamples := C.GoBytes(unsafe.Pointer(&pcm.samples[0][0]), C.int(length*4)) //32 bits each
-	rSamples := C.GoBytes(unsafe.Pointer(&pcm.samples[1][0]), C.int(length*4)) //32 bits each
-	lBuf := bytes.NewReader(lSamples)
-	rBuf := bytes.NewReader(rSamples)	
 	
-	for i:=0;i<length;i++{
-		//Read in a signed 32 bit sample
-		var l int32
-		var r int32
+	//lSamples := sliceFromArray(unsafe.Pointer(&pcm.samples[0][0]),length*4)
+	//rSamples := sliceFromArray(unsafe.Pointer(&pcm.samples[1][0]),length*4)
+	
+	lSamples :=  pcm.samples[0]
+	rSamples :=  pcm.samples[1]
+	
+	for i:=0;i<length;i++{		
+        sampleL := scale(int32(lSamples[i]))*4//audioLinearDither(16,lSamples[i], d.dither);
+		d.output.WriteByte(byte(sampleL>>8))
+		d.output.WriteByte(byte(sampleL>>0))
 		
-		binary.Read(lBuf,binary.LittleEndian,&l)
-		binary.Read(rBuf,binary.LittleEndian,&r)
-		
-		//Convert to 16 bit sample
-		l16 := scale(l)
-		r16 := scale(r)
-		
-		d.output<-l16
-		d.output<-r16
+        sampleR := scale(int32(rSamples[i]))*4//audioLinearDither(16, rSamples[i], d.dither);
+		d.output.WriteByte(byte(sampleR>>8))
+		d.output.WriteByte(byte(sampleR>>0))
 	}
+	
+	d.outputCount += length
 
 	return C.MAD_FLOW_CONTINUE
 } 
@@ -183,6 +199,63 @@ func errorFunc(data unsafe.Pointer, stream *C.struct_mad_stream, frame *C.struct
 	}
 }
 
+type audio_dither struct{
+	err [3]C.mad_fixed_t
+	rand C.mad_fixed_t
+}
+
+func prng(state uint32)(uint32){
+	return (state * 0x0019660d + 0x3c6ef35f) & 0xffffffff
+}
+
+func audioLinearDither(bits uint, sample C.mad_fixed_t,dither *audio_dither)(int32){
+	var scalebits uint
+	var output C.mad_fixed_t
+	var mask C.mad_fixed_t
+	var random C.mad_fixed_t
+
+	/* noise shape */
+	sample += dither.err[0] - dither.err[1] + dither.err[2];
+
+	dither.err[2] = dither.err[1];
+	dither.err[1] = dither.err[0] / 2;
+
+	/* bias */
+	output = sample + (1 << (C.MAD_F_FRACBITS + 1 - bits - 1));
+
+	scalebits = C.MAD_F_FRACBITS + 1 - bits;
+	mask = (1 << scalebits) - 1;
+
+	/* dither */
+	random  = C.mad_fixed_t(prng(uint32(dither.rand)))
+	output += (random & mask) - (dither.rand & mask);
+
+	dither.rand = random;
+
+	/* clip */
+	if (output > math.MaxInt16) {
+		output = math.MaxInt16;
+
+		if (sample > math.MaxInt16){
+			sample = math.MaxInt16;
+		}
+	}else if (output < math.MinInt16) {
+		output = math.MinInt16;
+
+		if (sample < math.MinInt16){
+			sample = math.MinInt16;
+		}
+	}
+
+	/* quantize */
+	output &= ^mask;
+
+	/* error feedback */
+	dither.err[0] = sample - output;
+
+	/* scale */
+	return int32(output >> scalebits);	
+}
 
 func scale(i int32)(int16){
 	if i >= math.MaxInt16{
@@ -192,4 +265,14 @@ func scale(i int32)(int16){
 	}else{
 		return int16(i >> (uint(C.MAD_F_FRACBITS) - 15))
 	}
+}
+
+func sliceFromArray(p unsafe.Pointer,length int)([]byte){
+        var theGoSlice []byte
+        sliceHeader := (*reflect.SliceHeader)((unsafe.Pointer(&theGoSlice)))
+        sliceHeader.Cap = length
+        sliceHeader.Len = length
+        sliceHeader.Data = uintptr(p)
+	
+	return theGoSlice
 }
